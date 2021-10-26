a6c37c405708364c8de08a3cbe4ff3fa944aefeb
xiangruz <31047726+xiangruz@users.noreply.github.com>
2021-10-26 15:09:50 -0400

    Update gmm_compare.py

--- gmm_compare.py
+++ gmm_compare.py
@@ -5,19 +5,20 @@ from gmm_tf import GaussianMixture as GMM
 import numpy as np 
 import time 
 from sklearn.metrics import homogeneity_completeness_v_measure 
- 
-np.random.seed(42) 
+
+np.random.seed(42)
  
 n_feature = 32 
-n_components = 3 
-sampe_size = 100 
+n_components = 5 
+sampe_size = [100, 200, 300, 200, 100]
+scale = 1.1 
  
 X = [] 
  
 for i in range(n_components): 
     A = np.random.rand(n_feature, n_feature) 
     B = np.dot(A, A.transpose()) 
-    X.append(np.random.multivariate_normal(mean = np.random.normal(scale = 1.5, size = n_feature), cov = B, size = sampe_size)) 
+    X.append(np.random.multivariate_normal(mean = np.random.normal(scale = scale, size = n_feature), cov = B, size = sampe_size[i])) 
  
 X = np.concatenate(X) 
  
@@ -31,7 +32,7 @@ print('sklearn time:', ft - it)
 print('sklearn accuracy:', homogeneity_completeness_v_measure(Y, gmm_sklearn.predict(X))) 
  
  
-gmm_tf = GMM(n_components=n_components, random_state=0, n_init = 50) 
+gmm_tf = GMM(n_components=n_components, random_state=0, n_init = 50, eps = 1e-6) 
 it2 = time.time() 
 gmm_tf.fit(X) 
 ft2 = time.time() 
